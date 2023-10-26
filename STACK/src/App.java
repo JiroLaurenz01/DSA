7f7b8b28ea21354b868f0924982924999670201c
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-10-26 21:47:37 +0800

    UPDATE FOR THE STACK

--- STACK/src/App.java
+++ STACK/src/App.java
@@ -13,23 +13,32 @@ public class App {
          *          3. BACKTRACKING ALGORITHMS (MAZE, FILE DIRECTORIES)      
                     4. CALLING FUNCTIONS (CALL STACK)  */ 
         
-        Stack<String> stack = new Stack<String>();      
+        Stack<String> stack = new Stack<String>();
+        // Create a new stack of strings named "stack."
+
+        // Add four strings to the stack using the push() method.
         stack.push("League of Legends");
         stack.push("Crossfire");
         stack.push("Farlight");
         stack.push("Call of Duty Warzone");
 
+        // Call the PrintStack method to print the contents of the stack.
         PrintStack(stack);
 
+        // Call the PopAndPrint method to pop the top element from the stack and print it.
         PopAndPrint(stack);
 
+        // Call the PrintStack method again to print the updated contents of the stack.
         PrintStack(stack);
 
+        // Call the PrintLastElement method to print the top element of the stack without removing it.
         PrintLastElement(stack);
 
+        // Call the SearchElement method to search for the string "Mobile Legends" in the stack.
         SearchElement(stack, "Mobile Legends");
     }
 
+    // Define the PrintStack method to print the contents of the stack. It also checks if the stack is empty.
     static void PrintStack(Stack<String> stack){
         System.out.println("\nNEW STACK:");
 
@@ -39,15 +48,18 @@ public class App {
             System.out.println("The stack is empty");
     }
 
+    // Define the PopAndPrint method to pop the top element from the stack, store it in popHolder, and then print it.
     static void PopAndPrint(Stack<String> stack) {
         String popHolder = stack.pop();
         System.out.println("\nThe popped string is " + popHolder);
     }
 
+    // Define the PrintLastElement method to print the top element of the stack without removing it.
     static void PrintLastElement(Stack<String> stack) {
         System.out.println("\nThe peeked string is " + stack.peek());
     }
 
+    // Define the SearchElement method to search for a specific string in the stack and print its position if found or a message if not found.
     static void SearchElement(Stack<String> stack, String toFind) {
         int position = stack.search(toFind);
         
