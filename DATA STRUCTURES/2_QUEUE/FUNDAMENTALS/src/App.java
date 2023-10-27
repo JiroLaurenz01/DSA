36320e0e91fc317061846b172a81635f31ae322f
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-10-27 18:14:49 +0800

    INTRO FOR QUEUE

--- DATA STRUCTURES/2_QUEUE/FUNDAMENTALS/src/App.java
+++ DATA STRUCTURES/2_QUEUE/FUNDAMENTALS/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Queue;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*  QUEUE = "FIFO" DATA STRUCTURE. FIRST-IN FIRST-OUT (EX. A LINE OF PEOPLE)
+                     A COLLECTION DESIGNED FOR HOLDING ELEMENTS PRIOR TO PROCESSING
+                     LINEAR DATA STRUCTURE
+
+                     ADD    = enqueue, offer()
+                     REMOVE = dequeue, poll()
+         
+            USES OF STACKS:
+                    1. KEYBOARD BUFFER (LETTERS SHOULD APPEAR ON THE SCREEN IN THE ORDER THEY'RE PRESSED)
+                    2. PRINTER QUEUE (PRINT JOBS SHOULD BE COMPLETED IN ORDER)
+                    3. USED IN LINKED LISTS, PRIORITY QUEUE, BREADTH-FIRST SEARCH  */ 
     }
 }
