678da614435d6691b8c93691c7917a177785aec0
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-10-28 22:08:14 +0800

    UPDATE FOR THE LINKED LIST

--- DATA STRUCTURES/3_LINKED LIST/FUNDAMENTALS/src/App.java
+++ DATA STRUCTURES/3_LINKED LIST/FUNDAMENTALS/src/App.java
@@ -2,6 +2,25 @@ import java.util.LinkedList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*  LINKED LIST = NODES ARE IN 2 PARTS (DATA + ADDRESS)
+                          NODES ARE IN NON CONSECUTIVE MEMORY LOCATIONS
+                          ELEMENTS ARE LINKED USING POINTERS
+            
+            ADVANTAGES:
+                1. DYNAMIC DATA STRUCTURE (ALLOCATES NEEDED MEMORY WHILE RUNNING).
+                2. INSERTION AND DELETION OF NODES IS EASY. O(1)
+                3. NO/LOW MEMORY WASTE.
+
+            DISADVANTAGES:
+                1. GREATER MEMORY USAGE (ADDITIONAL POINTER).
+                2. NO RANDOM ACCESS OF ELEMENTS (NO INDICES).
+                3. ACCESSING / SEARCHING ELEMENTS IS MORE TIME CONSUMING. O(n)
+
+            USES OF LINKED LIST:
+                    1. IMPLEMENT STACKS / QUEUE
+                    2. GPS NAVIGATION
+                    3. MUSIC PLAYLIST  */ 
+
+        
     }
 }
